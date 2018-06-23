4412c2699e64b6b87d4afae5c117b9016ee844c0
evilfeifei <1254288433@qq.com>
2018-06-23 20:06:43 +0800

    cc

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-console.log('begin');
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-console.log('end');
 var cacheStorageKey = 'minimal-pwa-1'
 var cacheList=[
   '/',
